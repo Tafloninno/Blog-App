1f0e11040dd0e98e4e34202ee414662e8c81e13d
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2023-06-10 15:13:24 +0300

    Made changes

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+  let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+
   subject { user.posts.create(title: 'Coding in progress', text: 'One line at a time') }
 
   describe 'validations' do
@@ -20,12 +21,12 @@ RSpec.describe Post, type: :model do
       expect(subject).to_not be_valid
     end
 
-    it 'comments_counter should be an interger' do
+    it 'comments_counter should be an integer' do
       subject.comments_counter = ''
       expect(subject).to_not be_valid
     end
 
-    it 'likes_counter should be an interger' do
+    it 'likes_counter should be an integer' do
       subject.likes_counter = ''
       expect(subject).to_not be_valid
     end
@@ -42,10 +43,9 @@ RSpec.describe Post, type: :model do
   end
 
   describe '#recent_comments' do
-    commenter2 = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-                             bio: 'Teacher from Poland.')
+    let(:commenter2) { User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.') }
 
-    it 'return most recents 5 comments' do
+    it 'returns the most recent 5 comments' do
       subject.comments.create(author: commenter2, text: 'Hi Tom!')
       comment2 = subject.comments.create(author: user, text: 'Hi Lilly!')
       comment3 = subject.comments.create(author: commenter2, text: 'Hope you all done with your project?')
@@ -56,6 +56,17 @@ RSpec.describe Post, type: :model do
 
       expect(recent_comments).to contain_exactly(comment6, comment5, comment4, comment3, comment2)
       expect(recent_comments.length).to eq(5)
+    end
+  end
+
+  describe '#update_posts_counter' do
+    it 'updates the posts_counter on the associated author after save' do
+      user.update(posts_counter: 0)
+
+      expect do
+        user.posts.create(title: 'New Post', text: 'Content')
+        user.reload
+      end.to change { user.posts_counter }.by(1)
     end
   end
 end
