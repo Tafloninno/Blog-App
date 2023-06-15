e95c3ceb920ea28aa6d6d261b4c70eec2dbc9796
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2023-06-15 16:38:09 +0300

    Update user and post cotrollers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
+    def index
+      @users = User.all
+      @current_user = current_user
+    end
+  
+    def show
+      @users = User.find(params[:id])
+      @posts = Post.find(params[:id])
+    end
   end
-
-  def show
-    @users = User.find(params[:id])
-    @posts = Post.find(params[:id])
-  end
-end
